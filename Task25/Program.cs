8cf1814a740cebb5675934cad91c532c231aa0b9
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-08-05 15:21:25 +0300

    add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,15 @@
+﻿int ExponentiationNum (int a, int b)
+{
+    int res = a;
+    for (int count = 1; count < b; count++)
+    {
+        res = res*a;     
+    }
+    return res;
+}
+
+Console.WriteLine("Введите два числа");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+int num_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(ExponentiationNum(num_1, num_2));
