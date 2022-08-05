15e306868994246ae47faa3c85845bece102dd27
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-08-05 15:25:46 +0300

    add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumOfDigits(int a)
+{
+    int res = 0;
+    while (a != 0)
+    {
+        res = res + a % 10;
+        a = a / 10;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(SumOfDigits(num));
